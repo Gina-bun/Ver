9e9345f1a9f05bded2a093c75b95a5a640808575
Gina-bun <64642337+Gina-bun@users.noreply.github.com>
2026-06-11 19:22:31 +0000

    feat: add navbar sub-links

--- src/script.js
+++ src/script.js
@@ -4,3 +4,20 @@ const mobileMenu = document.getElementById('mobile-menu');
 
 menuBtn.addEventListener('click', () => mobileMenu.classList.remove('hidden'));
 closeMenuBtn.addEventListener('click', () => mobileMenu.classList.add('hidden'));
+
+function toggleDropdown(name) {
+    const dropdown = document.getElementById(`dropdown-${name}`);
+    const chevron = document.getElementById(`chevron-${name}`);
+  
+    const isOpen = !dropdown.classList.contains('hidden');
+  
+    if (isOpen) {
+      dropdown.classList.add('hidden');
+      dropdown.classList.remove('flex');
+      chevron.style.transform = 'rotate(0deg)';
+    } else {
+      dropdown.classList.remove('hidden');
+      dropdown.classList.add('flex');
+      chevron.style.transform = 'rotate(90deg)';
+    }
+  }
